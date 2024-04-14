cd00b7a4b5330cf05216332a64fe74e920f9000d
Miqueias Andrade <miqueiasandr@gmail.com>
2024-04-13 22:12:55 -0300

    iniciando Banner da Home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,16 +1,35 @@
-import { BannerContainer } from './styles'
+import { BannerContainer, BoxTextBannerContainer } from './styles'
 import BannerImage from '../../assets/BannerImage.png'
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
 
 export function Home() {
   return (
     <BannerContainer>
-      <div>
-        <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-        <h2>
-          Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
-          hora
-        </h2>
-      </div>
+      <BoxTextBannerContainer>
+        <div>
+          <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+          <h2>
+            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+            hora
+          </h2>
+        </div>
+        <div>
+          <span>
+            <ShoppingCart weight="fill" size={16} />
+            Compra simples e segura
+          </span>
+          <span>
+            <Package weight="fill" size={16} />
+          </span>
+          <span>
+            <Timer weight="fill" size={16} />
+          </span>
+          <span>
+            <Coffee weight="fill" size={16} />
+          </span>
+        </div>
+      </BoxTextBannerContainer>
+
       <img src={BannerImage} alt="Copo de café bem grande" />
     </BannerContainer>
   )
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -4,13 +4,69 @@ export const BannerContainer = styled.div`
   display: flex;
   max-width: 70rem;
   margin: 0 auto;
-  align-items: center;
-  justify-content: center;
+  align-items: flex-start;
+  justify-content: space-between;
+  padding: 5.75rem 0;
+`
+
+export const BoxTextBannerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 36.75rem;
 
   div {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
     h1 {
-      font-family: 'Baloo';
-      color: red;
+      font-family: 'Baloo 2', sans-serif;
+      font-weight: 900;
+      font-size: 3rem;
+      line-height: 1.3;
+      color: ${(props) => props.theme['base-title']};
+    }
+
+    h2 {
+      font-family: 'Roboto', sans-serif;
+      font-size: 1.25rem;
+      color: ${(props) => props.theme['base-subTitle']};
+      line-height: 1.3;
+      font-weight: 400;
+    }
+  }
+
+  div + div {
+    display: grid;
+    grid-template-columns: 14.4375rem, 18.375rem;
+
+    span {
+      position: relative;
+      svg {
+        color: ${(props) => props.theme.background};
+        position: relative;
+        top: 8px;
+        left: 8px;
+      }
+      &::before {
+        content: '';
+        position: absolute;
+        width: 2rem;
+        height: 2rem;
+        border-radius: 50%;
+        background: ${(props) => props.theme['yellow-dark']};
+        left: 0;
+        top: 0;
+      }
+    }
+
+    span:nth-child(2)::before {
+      background: ${(props) => props.theme['base-text']};
+    }
+    span:nth-child(3)::before {
+      background: ${(props) => props.theme.yellow};
+    }
+    span:nth-child(4)::before {
+      background: ${(props) => props.theme.purple};
     }
   }
 `
