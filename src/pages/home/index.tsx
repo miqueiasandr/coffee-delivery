0d43869ac0b9b461bfdff9de4629051ce5d0338a
Miqueias Andrade <miqueiasandr@gmail.com>
2024-04-16 22:31:56 -0300

    Banner Home Concluído

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,36 +1,59 @@
-import { BannerContainer, BoxTextBannerContainer } from './styles'
+import {
+  BannerContainer,
+  BoxTextBannerContainer,
+  HomeContainer,
+  IconBannerCoffee,
+  IconBannerPackage,
+  IconBannerShoppingCart,
+  IconBannerTimer,
+} from './styles'
 import BannerImage from '../../assets/BannerImage.png'
 import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
 
 export function Home() {
   return (
-    <BannerContainer>
-      <BoxTextBannerContainer>
-        <div>
-          <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-          <h2>
-            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
-            hora
-          </h2>
-        </div>
-        <div>
-          <span>
-            <ShoppingCart weight="fill" size={16} />
-            Compra simples e segura
-          </span>
-          <span>
-            <Package weight="fill" size={16} />
-          </span>
-          <span>
-            <Timer weight="fill" size={16} />
-          </span>
-          <span>
-            <Coffee weight="fill" size={16} />
-          </span>
-        </div>
-      </BoxTextBannerContainer>
+    <HomeContainer>
+      <BannerContainer>
+        <BoxTextBannerContainer>
+          <div>
+            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+            <h2>
+              Com o Coffee Delivery você recebe seu café onde estiver, a
+              qualquer hora
+            </h2>
+          </div>
 
-      <img src={BannerImage} alt="Copo de café bem grande" />
-    </BannerContainer>
+          <div>
+            <IconBannerShoppingCart>
+              <span>
+                <ShoppingCart weight="fill" size={16} />
+              </span>
+              <p>Compra simples e segura</p>
+            </IconBannerShoppingCart>
+
+            <IconBannerPackage>
+              <span>
+                <Package weight="fill" size={16} />
+              </span>
+              <p>Embalagem mantém o café intacto</p>
+            </IconBannerPackage>
+            <IconBannerTimer>
+              <span>
+                <Timer weight="fill" size={16} />
+              </span>
+              <p>Entrega rápida e rastreada</p>
+            </IconBannerTimer>
+            <IconBannerCoffee>
+              <span>
+                <Coffee weight="fill" size={16} />
+              </span>
+              <p>O café chega fresquinho até você</p>
+            </IconBannerCoffee>
+          </div>
+        </BoxTextBannerContainer>
+
+        <img src={BannerImage} alt="Copo de café bem grande" />
+      </BannerContainer>
+    </HomeContainer>
   )
 }
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,32 +1,33 @@
 import { styled } from 'styled-components'
 
+export const HomeContainer = styled.div`
+  margin: 0 auto;
+  max-width: 70rem;
+`
+
 export const BannerContainer = styled.div`
   display: flex;
-  max-width: 70rem;
-  margin: 0 auto;
-  align-items: flex-start;
   justify-content: space-between;
-  padding: 5.75rem 0;
+  margin-top: 5.75rem;
 `
 
 export const BoxTextBannerContainer = styled.div`
   display: flex;
   flex-direction: column;
-  max-width: 36.75rem;
+  width: 36.75rem;
 
   div {
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
     h1 {
       font-family: 'Baloo 2', sans-serif;
       font-weight: 900;
       font-size: 3rem;
       line-height: 1.3;
+
       color: ${(props) => props.theme['base-title']};
     }
 
     h2 {
+      margin-top: 1rem;
       font-family: 'Roboto', sans-serif;
       font-size: 1.25rem;
       color: ${(props) => props.theme['base-subTitle']};
@@ -35,37 +36,68 @@ export const BoxTextBannerContainer = styled.div`
     }
   }
 
-  div + div {
+  > div + div {
+    margin-top: 4.125rem;
     display: grid;
-    grid-template-columns: 14.4375rem, 18.375rem;
+    grid-template-columns: 14.4275rem 1fr;
+    row-gap: 1.25rem;
+    column-gap: 2.5rem;
+  }
+`
 
-    span {
-      position: relative;
-      svg {
-        color: ${(props) => props.theme.background};
-        position: relative;
-        top: 8px;
-        left: 8px;
-      }
-      &::before {
-        content: '';
-        position: absolute;
-        width: 2rem;
-        height: 2rem;
-        border-radius: 50%;
-        background: ${(props) => props.theme['yellow-dark']};
-        left: 0;
-        top: 0;
-      }
+export const IconsBannerBase = styled.div`
+  display: flex;
+  align-items: center;
+  p {
+    margin-left: 0.75rem;
+    color: ${(props) => props.theme['base-text']};
+  }
+
+  span {
+    position: relative;
+    padding: 0.5rem;
+
+    &::before {
+      content: '';
+      position: absolute;
+      width: 2rem;
+      height: 2rem;
+      top: 0;
+      left: 0;
+      border-radius: 50%;
+      z-index: -1;
     }
 
-    span:nth-child(2)::before {
-      background: ${(props) => props.theme['base-text']};
+    svg {
+      color: ${(props) => props.theme.background};
     }
-    span:nth-child(3)::before {
+  }
+`
+
+export const IconBannerShoppingCart = styled(IconsBannerBase)`
+  span {
+    &::before {
+      background: ${(props) => props.theme['yellow-dark']};
+    }
+  }
+`
+export const IconBannerTimer = styled(IconsBannerBase)`
+  span {
+    &::before {
       background: ${(props) => props.theme.yellow};
     }
-    span:nth-child(4)::before {
+  }
+`
+export const IconBannerPackage = styled(IconsBannerBase)`
+  span {
+    &::before {
+      background: ${(props) => props.theme['base-text']};
+    }
+  }
+`
+export const IconBannerCoffee = styled(IconsBannerBase)`
+  span {
+    &::before {
       background: ${(props) => props.theme.purple};
     }
   }
